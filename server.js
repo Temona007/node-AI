3b11c7f7250fc53d3788ae043b26bb9a62547a57
Artem <temona007@gmail.com>
2024-06-06 09:00:25 +0300

    eqwe
    qwe

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config({ path: './config.env' });
+
+const port = process.env.PORT || 4242;
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+// Serve static files from the React app's build directory
+app.use(express.static(path.join(__dirname, 'my-react-app/build')));
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'my-react-app/build', 'index.html'));
+});
+
+// Handle other routes and return the React app's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'my-react-app/build', 'index.html'));
+});
+
+// Define a route for /about
+app.get('/api/about', (req, res) => {
+  res.send('About Page');
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
